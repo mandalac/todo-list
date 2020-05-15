88460764c977a447f560011bb99562df947f5ca0
Vasili Mandalac <vasilimandalac@Vasilis-MacBook-Pro.local>
2020-05-14 22:58:24 -0500

    initial

---
+++ docs/build/precache-manifest.c6efa805ce55740620178d5355f66e9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bd2c389bfa28bb8aecfa86de5f8791",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2532f3b6bd91ef9b8e62",
+    "url": "/todo-list/static/css/main.ebe84a66.chunk.css"
+  },
+  {
+    "revision": "ad4968b796b310dba0af",
+    "url": "/todo-list/static/js/2.0d787078.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.0d787078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2532f3b6bd91ef9b8e62",
+    "url": "/todo-list/static/js/main.4164a17d.chunk.js"
+  },
+  {
+    "revision": "0e635de1e5e33cbe5d76",
+    "url": "/todo-list/static/js/runtime-main.072a9b53.js"
+  }
+]);
